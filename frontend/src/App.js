ae735939fcb4b84e7821ace6130f73b0f50a5554
Dan <daniel.h.funk@gmail.com>
2022-09-20 15:03:16 -0400

    initial commit, with default build of FastAPI as a backend, and default React project as a frontend.

---
+++ frontend/src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo2.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to the SpiffWorkflow Demo Application
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Edit the Diagram
+        </a>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Run The Diagram
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
